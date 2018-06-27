fc4d407c1b19e1fa05a775e43df2d3deffc6c4fd
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-06-27 14:50:08 +0200

    feature/[#28] Added loading of the menu into categories in the MenuViewController.

--- restaurant-order/controllers/MenuViewController.swift
+++ restaurant-order/controllers/MenuViewController.swift
@@ -9,24 +9,50 @@
 import UIKit
 import RxSwift
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var menuTable: UITableView!
+    
     private let restaurantViewModel = RestaurantViewModel.shared
+    
+    private var restaurant: Restaurant?
     
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        menuTable.dataSource = self
 
         restaurantViewModel.selectedRestaurant
             .asObservable()
             .subscribe { [unowned self] in
                 if let restaurant = $0.element {
-                    print(restaurant)
+                    self.restaurant = restaurant
                 } else {
                     self.restaurantViewModel.loadRestaurantFromRealm()
                 }
             }
             .disposed(by: disposeBag)
         
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return restaurant?.menu?.categories?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurant?.menu?.categories?[section].items?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = restaurant?.menu?.categories?[indexPath.section].items?[indexPath.row].name ?? ""
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return restaurant?.menu?.categories?[section].name ?? ""
     }
 }
